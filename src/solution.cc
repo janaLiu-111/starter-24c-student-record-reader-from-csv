f2d736f37f2af77818fb605e1ec88cab282d2fa8
janaLiu-111 <s21082.liu@stu.scie.com.cn>
2026-02-10 22:51:36 -0600

    Refactor ReadStudentRecFromStream to use input stream
    Refactor ReadStudentRecFromStream to read from provided input stream instead of a file. Added error handling for reading first name, last name, UIN, and GPA.

--- src/solution.cc
+++ src/solution.cc
@@ -1,25 +1,54 @@
 #include "solution.hpp"
+#include <fstream>
+#include <string>
 
 Student ReadStudentRecFromStream(std::istream& is) {
-  const std::string input_file_name("./data.input.txt"); // assumes this file is within our working directory
-  std::ifstream ifs{input_file_name};
-  if (!ifs.is_open()) {
-    std::cerr << "Could not open " << input_file_name << std::endl;
-    return 1;
-  }
+    std::string first_name;
+    std::string last_name;
+    unsigned int uin;
+    double gpa;
+    char comma;
 
-
-  while(ifs.good()) {
-    unsigned int uin = 0;
-    double gpa = 0.0;
-    std::string full_name;
-    char throw_away = '\0';
-    
-    ifs >> full_name >> throw_away >> uin >> throw_away >> gpa;
-    if (ifs.fail()){
-      break;
-      return Student{};
+    // Read first name
+    is >> first_name;
+    if (is.fail()) {
+        return Student{};
     }
-  }
-  return Student{full_name, uin, gpa};
+
+    // Read last name (will include trailing comma)
+    is >> last_name;
+    if (is.fail()) {
+        return Student{};
+    }
+
+    // Check that last name ends with comma
+    if (last_name.empty() || last_name.back() != ',') {
+        return Student{};
+    }
+
+    // Remove comma
+    last_name.pop_back();
+
+    // Read UIN
+    is >> uin;
+    if (is.fail()) {
+        return Student{};
+    }
+
+    // Read comma after UIN
+    is >> comma;
+    if (is.fail() || comma != ',') {
+        return Student{};
+    }
+
+    // Read GPA
+    is >> gpa;
+    if (is.fail()) {
+        return Student{};
+    }
+
+    // Combine names
+    std::string full_name = first_name + " " + last_name;
+
+    return Student{full_name, uin, gpa};
 }
