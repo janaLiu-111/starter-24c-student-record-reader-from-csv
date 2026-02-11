ab5b562c613788231c28311be38371a8e85c974f
janaLiu-111 <s21082.liu@stu.scie.com.cn>
2026-02-10 22:45:08 -0600

    Add function to read student records from file
    Implemented ReadStudentRecFromStream to read student records from a file.

--- src/solution.cc
+++ src/solution.cc
@@ -1,6 +1,25 @@
 #include "solution.hpp"
 
 Student ReadStudentRecFromStream(std::istream& is) {
-  // your implementation here...
-  return Student{};
+  const std::string input_file_name("./data.input.txt"); // assumes this file is within our working directory
+  std::ifstream ifs{input_file_name};
+  if (!ifs.is_open()) {
+    std::cerr << "Could not open " << input_file_name << std::endl;
+    return 1;
+  }
+
+
+  while(ifs.good()) {
+    unsigned int uin = 0;
+    double gpa = 0.0;
+    std::string full_name;
+    char throw_away = '\0';
+    
+    ifs >> full_name >> throw_away >> uin >> throw_away >> gpa;
+    if (ifs.fail()){
+      break;
+      return Student{};
+    }
+  }
+  return Student{full_name, uin, gpa};
 }
